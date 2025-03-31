d715d3351ebf547028a2983aed1ac7f48b6d5a11
santiago campos <campos.santiago138@gmail.com>
2025-03-31 12:20:34 -0400

    modified banner code to fit and move slower on when the user loads the site.i re organized the css and js into the styles.css and script.js files out of the index.html file

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("content").classList.remove("hidden");
     }, 2000); // Simulates loading delay
 });
+
+
+document.getElementById("menu-btn").addEventListener("click", function(event) {
+    event.stopPropagation();
+    this.classList.add("rotate-180");
+    document.getElementById("menu").style.right = "0";
+});
+
+document.getElementById("close-btn").addEventListener("click", function(event) {
+    event.stopPropagation();
+    document.getElementById("menu-btn").classList.remove("rotate-180");
+    document.getElementById("menu").style.right = "-100%";
+});
+
+function closeMenu(event) {
+    let menu = document.getElementById("menu");
+    let menuBtn = document.getElementById("menu-btn");
+    if (!menu.contains(event.target) && event.target !== menuBtn) {
+        menu.style.right = "-100%";
+        menuBtn.classList.remove("rotate-180");
+    }
+}
+
+// banner // 
+
