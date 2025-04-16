aedc3ef8c7fc309b017cb2945b58324a13f092ef
santiago campos <campos.santiago138@gmail.com>
2025-04-16 13:55:50 -0400

    push on edits

--- script.js
+++ script.js
@@ -57,3 +57,68 @@ $(document).ready(function () {
       $("#rightPane").animate({ width: 0 }, 1000);
     });
   });
+
+
+
+  // tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        keyframes: {
+          'fade-in': {
+            '0%': { opacity: 0 },
+            '100%': { opacity: 1 },
+          },
+          'fade-in-left': {
+            '0%': { opacity: 0, transform: 'translateX(-20px)' },
+            '100%': { opacity: 1, transform: 'translateX(0)' },
+          },
+          'fade-in-up': {
+            '0%': { opacity: 0, transform: 'translateY(20px)' },
+            '100%': { opacity: 1, transform: 'translateY(0)' },
+          },
+        },
+        animation: {
+          'fade-in': 'fade-in 1s ease-out',
+          'fade-in-left': 'fade-in-left 1s ease-out',
+          'fade-in-up': 'fade-in-up 1s ease-out',
+        },
+      },
+    },
+    plugins: [],
+  };
+
+
+  module.exports = {
+    content: ["./*.html"],
+    theme: {
+      extend: {
+        animation: {
+          'pop': 'pop 0.7s cubic-bezier(0.26, 0.53, 0.74, 1.48) backwards',
+          'slide': 'slide 0.7s cubic-bezier(0.26, 0.53, 0.74, 1.48) backwards',
+          'slide-left': 'slide-left 0.7s cubic-bezier(0.26, 0.53, 0.74, 1.48) backwards',
+          'slide-up': 'slide-up 0.7s cubic-bezier(0.26, 0.53, 0.74, 1.48) backwards',
+        },
+        keyframes: {
+          pop: {
+            '0%': { opacity: '0', transform: 'scale(0.5)' },
+            '100%': { opacity: '1', transform: 'scale(1)' },
+          },
+          slide: {
+            '0%': { opacity: '0', transform: 'translateX(4em)' },
+            '100%': { opacity: '1', transform: 'translateX(0)' },
+          },
+          'slide-left': {
+            '0%': { opacity: '0', transform: 'translateX(-40px)' },
+            '100%': { opacity: '1', transform: 'translateX(0)' },
+          },
+          'slide-up': {
+            '0%': { opacity: '0', transform: 'translateY(3em)' },
+            '100%': { opacity: '1', transform: 'translateY(0)' },
+          },
+        }
+      }
+    },
+    plugins: [],
+  }
+  
